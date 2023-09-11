d0bbf0f7853154dec227582f9990134596d5ffd0
Debtanu Ghosh <debdebtanu@gmail.com>
2023-09-11 23:42:26 +0530

    initial commit

---
+++ app/Http/Controllers/EventCalendarController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EventCalendar;
+use App\Http\Requests\StoreEventCalendarRequest;
+use App\Http\Requests\UpdateEventCalendarRequest;
+
+class EventCalendarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreEventCalendarRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(EventCalendar $eventCalendar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(EventCalendar $eventCalendar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateEventCalendarRequest $request, EventCalendar $eventCalendar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(EventCalendar $eventCalendar)
+    {
+        //
+    }
+}
