fb05ebfc2eb98b3309d272be1735b33aac215b3e
debtanu-capitalnumbers <debtanu@capitalnumbers.com>
2024-01-23 04:39:56 +0530

    add msg

--- app/Http/Requests/UpdateEventRequest.php
+++ app/Http/Requests/UpdateEventRequest.php
@@ -37,4 +37,17 @@ class UpdateEventRequest extends FormRequest
             $rules['cover_image'] = 'required|mimes:png,jpg,jpeg|max:4096';
         }
     }
+
+    public function messages()
+    {
+        return [
+            'title.required' => 'Title is required',
+            'description.required' => 'Description is required',
+            'location.required' => 'Location is required',
+            'event_category.required' => 'Event category is required',
+            'event_start_date.required' => 'Event start date is required',
+            'event_start_time.required' => 'Event start time is required',
+            'event_end_time.required' => 'Event end time is required',
+        ];
+    }
 }
